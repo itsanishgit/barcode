68d50aeeb3e207a796ef7543ef19d89aadebbffa
Anish Kumar <itsanishmail@gmail.com>
2022-03-21 13:30:47 +0530

    Updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements AfterViewInit {
   @ViewChild(BarcodeScannerLivestreamComponent)
   barcodeScanner: any;
   barcodeValue: any;
+  allowedFormats = ['aztec', 'code_128', 'code_39', 'code_93', 'codabar', 'data_matrix', 'ean_13', 'ean_8', 'itf', 'pdf417', 'qr_code', 'upc_a', 'upc_e'];
 
   constructor(private cd: ChangeDetectorRef) {
     this.barcodeValue = [];
